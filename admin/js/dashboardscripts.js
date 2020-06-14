0c5d2730e951d9a8ecdaebdc4cd0ae4d71e4c4a2
AbaidooPrince <abaidooprince@gmail.com>
2020-06-14 07:45:14 +0000

    update before meeting

--- admin/js/dashboardscripts.js
+++ admin/js/dashboardscripts.js
@@ -1,4 +1,14 @@
 $(document).ready(function() {
+
     $('#dataTable').DataTable();
     $('.dataTables_length').addClass('bs-select');
+
+    $('#viewMessage').click(function() {
+        $('#message').toggle().slideDown();
+        // $('.viewMessage').toggle().hide();
+
+
+
+    });
+
 });
